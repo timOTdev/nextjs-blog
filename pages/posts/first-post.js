45bf096e71b36ed9472b1c88f8c4a67bb91cd017
timOTdev <tim@timothyhoang.dev>
2022-01-23 16:58:59 -0600

    Update component styles

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -17,11 +17,7 @@ export default function FirstPost() {
         }
       />
       <h1>First Post</h1>
-      <h2>
-        <Link href='/'>
-          <a>Back to home</a>
-        </Link>
-      </h2>
+      <p>This is where the content would go.</p>
     </Layout>
   );
 }
